f9b910b742477cfecdfb57ef19a51177ca5f1ce3
Hasan Fırat Keskin <hfk4747@gmail.com>
2025-08-17 21:19:40 +0300

    Upload

---
+++ OpenWave_V0.1_WinUI/body.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace OpenWave_V0._1_WinUI
+{
+    partial class body
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(body));
+            this.formsPlot1 = new ScottPlot.WinForms.FormsPlot();
+            this.rbManage = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.COMListBox = new System.Windows.Forms.ComboBox();
+            this.ListComPort = new System.Windows.Forms.Button();
+            this.PortConnect = new System.Windows.Forms.Button();
+            this.PortDisconnect = new System.Windows.Forms.Button();
+            this.TerminalEkran = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ADCRun = new System.Windows.Forms.Button();
+            this.ADCStop = new System.Windows.Forms.Button();
+            this.formsPlot2 = new ScottPlot.WinForms.FormsPlot();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // formsPlot1
+            // 
+            this.formsPlot1.DisplayScale = 0F;
+            this.formsPlot1.Location = new System.Drawing.Point(12, 80);
+            this.formsPlot1.Name = "formsPlot1";
+            this.formsPlot1.Size = new System.Drawing.Size(561, 288);
+            this.formsPlot1.TabIndex = 0;
+            // 
+            // rbManage
+            // 
+            this.rbManage.AutoSize = true;
+            this.rbManage.Location = new System.Drawing.Point(270, 14);
+            this.rbManage.Name = "rbManage";
+            this.rbManage.Size = new System.Drawing.Size(68, 17);
+            this.rbManage.TabIndex = 2;
+            this.rbManage.Text = "Axis Limit";
+            this.rbManage.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Checked = true;
+            this.radioButton2.Location = new System.Drawing.Point(344, 14);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(77, 17);
+            this.radioButton2.TabIndex = 2;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Auto Scale";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // COMListBox
+            // 
+            this.COMListBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.COMListBox.FormattingEnabled = true;
+            this.COMListBox.Location = new System.Drawing.Point(13, 10);
+            this.COMListBox.Name = "COMListBox";
+            this.COMListBox.Size = new System.Drawing.Size(75, 21);
+            this.COMListBox.TabIndex = 3;
+            // 
+            // ListComPort
+            // 
+            this.ListComPort.Location = new System.Drawing.Point(13, 37);
+            this.ListComPort.Name = "ListComPort";
+            this.ListComPort.Size = new System.Drawing.Size(75, 37);
+            this.ListComPort.TabIndex = 1;
+            this.ListComPort.Text = "Portları Listele";
+            this.ListComPort.UseVisualStyleBackColor = true;
+            this.ListComPort.Click += new System.EventHandler(this.ListComPort_Click);
+            // 
+            // PortConnect
+            // 
+            this.PortConnect.Enabled = false;
+            this.PortConnect.Location = new System.Drawing.Point(94, 10);
+            this.PortConnect.Name = "PortConnect";
+            this.PortConnect.Size = new System.Drawing.Size(75, 21);
+            this.PortConnect.TabIndex = 1;
+            this.PortConnect.Text = "Bağlan";
+            this.PortConnect.UseVisualStyleBackColor = true;
+            this.PortConnect.Click += new System.EventHandler(this.PortConnect_Click);
+            // 
+            // PortDisconnect
+            // 
+            this.PortDisconnect.Enabled = false;
+            this.PortDisconnect.Location = new System.Drawing.Point(94, 37);
+            this.PortDisconnect.Name = "PortDisconnect";
+            this.PortDisconnect.Size = new System.Drawing.Size(75, 37);
+            this.PortDisconnect.TabIndex = 1;
+            this.PortDisconnect.Text = "Bağlantıyı Kes";
+            this.PortDisconnect.UseVisualStyleBackColor = true;
+            this.PortDisconnect.Click += new System.EventHandler(this.PortDisconnect_Click);
+            // 
+            // TerminalEkran
+            // 
+            this.TerminalEkran.Location = new System.Drawing.Point(579, 37);
+            this.TerminalEkran.Multiline = true;
+            this.TerminalEkran.Name = "TerminalEkran";
+            this.TerminalEkran.Size = new System.Drawing.Size(118, 544);
+            this.TerminalEkran.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(576, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Terminal:";
+            // 
+            // ADCRun
+            // 
+            this.ADCRun.Enabled = false;
+            this.ADCRun.Location = new System.Drawing.Point(175, 10);
+            this.ADCRun.Name = "ADCRun";
+            this.ADCRun.Size = new System.Drawing.Size(75, 27);
+            this.ADCRun.TabIndex = 1;
+            this.ADCRun.Text = "Çalıştır";
+            this.ADCRun.UseVisualStyleBackColor = true;
+            this.ADCRun.Click += new System.EventHandler(this.ADCRun_Click);
+            // 
+            // ADCStop
+            // 
+            this.ADCStop.Enabled = false;
+            this.ADCStop.Location = new System.Drawing.Point(175, 43);
+            this.ADCStop.Name = "ADCStop";
+            this.ADCStop.Size = new System.Drawing.Size(75, 31);
+            this.ADCStop.TabIndex = 1;
+            this.ADCStop.Text = "Durdur";
+            this.ADCStop.UseVisualStyleBackColor = true;
+            this.ADCStop.Click += new System.EventHandler(this.ADCStop_Click);
+            // 
+            // formsPlot2
+            // 
+            this.formsPlot2.DisplayScale = 0F;
+            this.formsPlot2.Location = new System.Drawing.Point(13, 375);
+            this.formsPlot2.Name = "formsPlot2";
+            this.formsPlot2.Size = new System.Drawing.Size(560, 206);
+            this.formsPlot2.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(261, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(121, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Baskın Frekans (Hz)";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(388, 49);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "0 Hz";
+            // 
+            // body
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(713, 596);
+            this.Controls.Add(this.formsPlot2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TerminalEkran);
+            this.Controls.Add(this.COMListBox);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.rbManage);
+            this.Controls.Add(this.ADCStop);
+            this.Controls.Add(this.ADCRun);
+            this.Controls.Add(this.PortDisconnect);
+            this.Controls.Add(this.PortConnect);
+            this.Controls.Add(this.ListComPort);
+            this.Controls.Add(this.formsPlot1);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "body";
+            this.Text = "OpenWave v0.1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private ScottPlot.WinForms.FormsPlot formsPlot1;
+        private System.Windows.Forms.RadioButton rbManage;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.ComboBox COMListBox;
+        private System.Windows.Forms.Button ListComPort;
+        private System.Windows.Forms.Button PortConnect;
+        private System.Windows.Forms.Button PortDisconnect;
+        private System.Windows.Forms.TextBox TerminalEkran;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button ADCRun;
+        private System.Windows.Forms.Button ADCStop;
+        private ScottPlot.WinForms.FormsPlot formsPlot2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ OpenWave_V0.1_WinUI/body.cs
@@ -0,0 +1,302 @@
+﻿using ScottPlot;
+using ScottPlot.Plottables;
+using ScottPlot.Statistics;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows.Forms;
+using MathNet.Numerics.IntegralTransforms;
+
+namespace OpenWave_V0._1_WinUI
+{
+    public partial class body : Form
+    {
+
+        public string Title => "Data Streamer";
+        public string Description => "Plots live streaming data as a fixed-width line plot, " +
+            "shifting old data out as new data comes in.";
+
+        readonly System.Windows.Forms.Timer AddNewDataTimer = new System.Windows.Forms.Timer() { Interval = 10, Enabled = true };
+        readonly System.Windows.Forms.Timer UpdatePlotTimer = new System.Windows.Forms.Timer() { Interval = 50, Enabled = true };
+
+        readonly ScottPlot.Plottables.DataStreamer Streamer1;
+
+        readonly ScottPlot.DataGenerators.RandomWalker Walker1 = new ScottPlot.DataGenerators.RandomWalker(0);
+
+        public body()
+        {
+            InitializeComponent();
+
+            Streamer1 = formsPlot1.Plot.Add.DataStreamer(1024*TotalBlock);
+
+            Streamer1.LineColor = Colors.DarkRed;
+
+            formsPlot1.UserInputProcessor.Enable();
+            
+            UpdatePlotTimer.Tick += (s, e) =>
+            {
+                if (Streamer1.HasNewData)
+                {
+                    formsPlot1.Plot.Title($"Processed {Streamer1.Data.CountTotal:N0} points");
+                    formsPlot1.Refresh();
+                }
+            };
+            
+            Streamer1.ViewScrollLeft();
+
+            rbManage.CheckedChanged += (sender, e) =>
+            {
+                RadioButton rb = sender as RadioButton;
+                if (rb != null && rb.Checked)
+                {
+                    formsPlot1.Plot.Axes.ContinuouslyAutoscale = false;
+                    Streamer1.ManageAxisLimits = true;
+                }
+                else
+                {
+                    formsPlot1.Plot.Axes.ContinuouslyAutoscale = true;
+                    Streamer1.ManageAxisLimits = false;
+                }
+                formsPlot1.Refresh();
+            };
+
+            
+        }
+
+        public int TotalBlock = 5;
+        private SerialPort SeriPort = new SerialPort();
+        private byte[] buffer;
+        private List<byte> tempBuffer = new List<byte>();
+        private System.Timers.Timer timeoutTimer;
+        private const int TIMEOUT_MS = 40; // ms cinsinden
+
+        private void ADCRun_Click(object sender, EventArgs e)
+        {
+            ADCRun.Enabled = false;
+            ADCStop.Enabled = true;
+            PortDisconnect.Enabled = false;
+
+            
+            SeriPort.Write("Start");
+
+            SeriPort.BaseStream.ReadAsync(buffer, 0, buffer.Length);
+
+        }
+
+        private void ADCStop_Click(object sender, EventArgs e)
+        {
+            ADCRun.Enabled = true;
+            ADCStop.Enabled = false;
+            PortDisconnect.Enabled = true;
+            SeriPort.Write("Stop!");
+        }
+
+        private void PortDisconnect_Click(object sender, EventArgs e)
+        {
+            SeriPort.Close();
+            PortDisconnect.Enabled = false;
+            PortConnect.Enabled = true;
+            ADCRun.Enabled = false;
+            ADCStop.Enabled = false;
+        }
+
+        private void PortConnect_Click(object sender, EventArgs e)
+        {
+            if (COMListBox.SelectedItem != null)
+            {
+                SeriPort.PortName = COMListBox.SelectedItem.ToString();
+                SeriPort.BaudRate = 115200;
+                buffer = new byte[(TotalBlock * 1024 * 2)];
+                SeriPort.ReadBufferSize = buffer.Length;
+                SeriPort.DataReceived += SeriPort_DataReceived;
+
+                timeoutTimer = new System.Timers.Timer(TIMEOUT_MS);
+                timeoutTimer.AutoReset = false;
+                timeoutTimer.Elapsed += TimeoutTimer_Elapsed;
+
+                try
+                {
+                    SeriPort.Open();
+                    TerminalEkran.AppendText("Port açıldı! \n");
+                    PortDisconnect.Enabled = true;
+                    PortConnect.Enabled = false;
+                    ADCRun.Enabled = true;
+                }
+                catch (Exception ex)
+                {
+                    TerminalEkran.AppendText("Port açılamadı:\n" + ex.Message + "\n");
+                }
+            }
+        }
+
+        private void SeriPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            int bytesAvailable = SeriPort.BytesToRead;
+            if (bytesAvailable > 0)
+            {
+                byte[] readBuffer = new byte[bytesAvailable];
+                SeriPort.Read(readBuffer, 0, bytesAvailable);
+
+                tempBuffer.AddRange(readBuffer);
+
+                // Tampon dolduysa
+                if (tempBuffer.Count >= buffer.Length)
+                {
+                    OnBufferFull(tempBuffer.ToArray(), tempBuffer.Count);
+                    tempBuffer.Clear();
+                }
+
+                timeoutTimer.Stop();
+                timeoutTimer.Start();
+            }
+        }
+
+        private void OnBufferFull(byte[] data, int length)
+        {
+            this.BeginInvoke((Action)(() =>
+            {
+                var sb = new StringBuilder();
+                int sampleCount = length / 2;
+                ushort[] samples16 = new ushort[sampleCount];
+
+                for (int i = 0, j = 0; i + 1 < length; i += 2, j++)
+                {
+                    ushort value = BitConverter.ToUInt16(data, i); // little endian
+                    sb.AppendLine(value.ToString());
+                    samples16[j] = value;
+
+                    // Grafik için ekle
+                    Streamer1.Add(value);
+                }
+
+                // ushort -> double
+                double[] samples = samples16.Select(s => (double)s).ToArray();
+
+                // FFT hesapla ve ScottPlot ile çiz
+                ComputeFFT(samples);
+                AppendToTerminalSafe(sb.ToString());
+            }));
+        }
+
+        private void AppendToTerminalSafe(string text, int maxLines = 1000)
+        {
+            this.BeginInvoke((Action)(() =>
+            {
+                // Mevcut satırlar + yeni satırlar
+                var allLines = TerminalEkran.Lines.ToList();
+                allLines.AddRange(text.Split('\n'));
+
+                // Son maxLines satırı al
+                if (allLines.Count > maxLines)
+                {
+                    allLines = allLines.Skip(allLines.Count - maxLines).ToList();
+                }
+
+                TerminalEkran.Text = string.Join("\n", allLines);
+
+                // Scroll otomatik
+                TerminalEkran.SelectionStart = TerminalEkran.Text.Length;
+                TerminalEkran.ScrollToCaret();
+            }));
+        }
+
+
+        private void TimeoutTimer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            if (tempBuffer.Count > 0)
+            {
+                this.BeginInvoke((Action)(() =>
+                {
+                    TerminalEkran.AppendText($"Timeout! Tampon temizlendi, {tempBuffer.Count} byte atıldı.\n");
+                }));
+                tempBuffer.Clear();
+            }
+        }
+
+        private void ListComPort_Click(object sender, EventArgs e)
+        {
+            string[] ports = SerialPort.GetPortNames();
+
+            COMListBox.Items.Clear();
+
+            foreach (string port in ports)
+            {
+                COMListBox.Items.Add(port);
+            }
+
+            if (COMListBox.Items.Count > 0)
+            {
+                COMListBox.SelectedIndex = 0;
+                PortConnect.Enabled = true;
+            }
+                
+            else
+            {
+                PortConnect.Enabled = false;
+            }                
+        }
+        
+        private void ComputeFFT(double[] samples)
+        {
+            int N = samples.Length; // Örnek sayısı
+            double sampleRate = 1.0 / (1.1666e-6); // ~857 kHz
+
+            // DC offset'i kaldır
+            double mean = samples.Average();
+            for (int i = 0; i < samples.Length; i++)
+                samples[i] -= mean;
+
+            // Pencere uygulama (opsiyonel ama önerilir)
+            var window = MathNet.Numerics.Window.Hann(samples.Length);
+            for (int i = 0; i < samples.Length; i++)
+                samples[i] *= window[i];
+
+            // double[] -> Complex[] dönüştür
+            Complex[] spectrum = samples.Select(s => new Complex(s, 0)).ToArray();
+
+            // FFT hesapla
+            Fourier.Forward(spectrum, FourierOptions.Matlab);
+
+            // Genlik spektrumu
+            double[] magnitude = spectrum
+                .Take(N / 2)
+                .Select(c => c.Magnitude)
+                .ToArray();
+
+            // Frekans ekseni
+            double[] frequencies = Enumerable.Range(0, N / 2)
+                .Select(i => i * sampleRate / N)
+                .ToArray();
+
+            // DC bileşenini atlayarak baskın frekansı bul
+            int peakIndex = magnitude
+                .Skip(1)
+                .Select((val, idx) => new { val, idx })
+                .OrderByDescending(x => x.val)
+                .First().idx + 1; // +1 çünkü Skip(1)
+
+            double peakFrequency = frequencies[peakIndex];
+
+            // Konsola veya terminale yazdır
+            label3.Text = $"{peakFrequency:N2} Hz";
+
+            // ScottPlot ile çiz
+            formsPlot2.Plot.Clear();
+            formsPlot2.Plot.Add.SignalXY(frequencies, magnitude);
+            formsPlot2.Plot.Title("FFT Spektrumu");
+            formsPlot2.Plot.XLabel("Frekans (Hz)");
+            formsPlot2.Plot.YLabel("Genlik");
+            formsPlot2.Refresh();
+        }
+       
+    }
+}
